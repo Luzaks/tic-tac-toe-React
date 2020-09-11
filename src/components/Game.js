1b9d0efb8af60946969c580bbb04f5e02ed98c6d
Luzaks <kanemekanik@gmail.com>
2020-09-10 20:51:58 -0500

    [Style] Add background particles.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,18 +1,22 @@
 import React from "react";
 import Board from './Board/Board';
+import ParticlesBg from 'particles-bg'
 
 class Game extends React.Component {
     render() {
         return (
-            <div className="game">
-                <div className="game-board">
-                    <Board />
+            <React.Fragment>
+                <div className="game">
+                    <div className="game-board">
+                        <Board />
+                    </div>
+                    <div className="game-info">
+                        <div>{/* status */}</div>
+                        <ol>{/* TODO */}</ol>
+                    </div>
                 </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
+            <ParticlesBg type="circle" bg={true} />
+        </React.Fragment>
         );
     }
 }
